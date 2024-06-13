6ed4b8a49292fcad55b407198d9bc1459186befa
Sanskar Modi <sansyprog8@gmail.com>
2024-06-13 18:56:07 +0530

    Update prompt.py to include chat_history

--- src/prompt.py
+++ src/prompt.py
@@ -3,6 +3,7 @@ Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 Context: {context}
+Chat History: {chat_history)
 Question: {question}
 
 Only return the helpful answer below and nothing else.
