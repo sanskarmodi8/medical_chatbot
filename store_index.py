0862464719aa1d2402f8b909f2d7605206454802
Sanskar Modi <sansyprog8@gmail.com>
2024-06-13 20:07:20 +0530

    Update store_index.py

--- store_index.py
+++ store_index.py
@@ -84,4 +84,4 @@ def langchain_pinecone_from_texts_custom_updated(
     return Pinecone(index, embedding.embed_query, text_key, namespace)
         
 docsearch = langchain_pinecone_from_texts_custom_updated([t.page_content for t in text_chunks], embeddings, index_name="medical-chatbot")
-# docsearch.__class__ = CustomPinecone
+docsearch.__class__ = CustomPinecone
