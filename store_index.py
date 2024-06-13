bb5c042a0cb213a7eca7c5bd97897d5b58fd6ac6
Sanskar Modi <sansyprog8@gmail.com>
2024-06-13 20:43:37 +0530

    Update store_index.py

--- store_index.py
+++ store_index.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 import pickle
 import uuid
-from src.custom_pinecone import CustomPinecone
+# from src.custom_pinecone import CustomPinecone
 
 load_dotenv()
 
